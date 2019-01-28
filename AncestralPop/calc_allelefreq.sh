08e962eccbe626daec44eb12c545749afbe44ef1
dcruz <diana.cruz@sib.swiss>
2019-01-28 16:46:58 +0100

    get sites and genotypes on ancient sample

--- AncestralPop/calc_allelefreq.sh
+++ AncestralPop/calc_allelefreq.sh
@@ -10,3 +10,14 @@ p=32
  angsd -out alleles_maf${minmaf} -doMaf 3 \
        -doMajorMinor 1 -anc $anc.fa \
        -bam bam.filelist -GL 1 -minMaf $minmaf -p $p
+
+# Determine the variant sites in the reference population
+gunzip -c alleles_maf${minmaf}.mafs.gz | cut -f 1,2 |sed 1d > sites_${minmaf}.txt
+angsd sites index sites_${minmaf}.txt 
+angsd sites print sites_${minmaf}.txt
+
+# "Ancient" individual to test
+ind=MN0008
+angsd -dogeno 4 -doPost 2 -postCutoff 0.95 \
+  -doMajorMinor 1 -doMaf 2 -i ${ind}.bam \
+  -out genos_${ind} -GL 1
