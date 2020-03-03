14b63b4efc215ea56d91deab44b78cb2308eabbb
dcruz <diana-ivette.cruz-davalos@unil.ch>
2020-03-03 11:33:20 +0100

    try to simplify script; update scp address

--- Admixture/find_likelihood.R
+++ Admixture/find_likelihood.R
@@ -1,28 +1,30 @@
 args = commandArgs(trailingOnly=TRUE)
 
-preffix <- args[1]
+prefix <- args[1]
 panel_name <- args[2]
-ind_name <- args[3]
+# ind_name <- args[3]
+min_k <- args[3]
 n <- args[4]
 program <- args[5]
 
-suffix <- paste( panel_name, ind_name, sep = "_")
-files <- list.files(".", pattern=paste(preffix, suffix, sep = ".*"))
+# sufix <- paste( panel_name, ind_name, sep = "_")
+files <- list.files(".", pattern = paste(panel_name, sep = ".*"))
 length(files)
 toscp <- c()
 ourbest <- c()
-for(k in seq(2, n)){
-  file_name <- paste(preffix, "_k", k, "_", panel_name, "_", ind_name, sep = "")
+for(k in seq(min_k, n)){
+  file_name <- paste(prefix, "_k", k, "_", panel_name, ".txt",#"_", ind_name, 
+  sep = "")
   file <- read.csv(file_name, sep = "\t", header = F)
   like <- max(file$V2)
   index <- which(file$V2 == like)
   print(paste("Best likelihood for k", k, "is", like, "of replicate", file$V1[index]))
 
   if(program == "ADMIXTURE"){
-    best <- paste(k, "/", panel_name, "_", sub(".txt", "",ind_name),
+    best <- paste(k, "/", panel_name, #"_", sub(".txt", "",ind_name),
                   "_",file$V1[index], ".", k, sep = "")
   }else{
-    best <- paste(k, "/", panel_name, "_", sub(".txt", "",ind_name),
+    best <- paste(k, "/", panel_name, #"_", sub(".txt", "",ind_name),
                   "_k", k, "_",file$V1[index], sep = "")
   }
 
@@ -35,17 +37,19 @@ ourbest <- sub(",", "", ourbest)
 toscp <- sub(",", "", toscp)
 curdir <- getwd()
 if(program == "ADMIXTURE"){
-  print(paste("scp dcruzdva@prd.vital-it.ch:", curdir, "/{",
+  print(paste("scp Axiom:", #dcruzdva@prd.vital-it.ch:", 
+  curdir, "/{",
         toscp, "}.Q ./", sep = ""))
 }else{
-  print(paste("scp dcruzdva@prd.vital-it.ch:", curdir, "/{",
-        toscp, "}.qopt ./", sep = ""))
+  print(paste("scp Axiom:", #dcruzdva@prd.vital-it.ch:", 
+              curdir, "/{",
+              toscp, "}.qopt ./", sep = ""))
 }
 
 if(program == "ADMIXTURE"){
   forloop <- paste("for(k in paste(c(",  ourbest,  "), seq(2,",  n, "), sep = '.'))"  )
 }else{
-  forloop <- paste("for(k in paste(seq(2,",  n,  "), c(",  ourbest,  "), sep = '_'))"  )
+  forloop <- paste("for(k in paste(seq(",min_k,",",  n,  "), c(",  ourbest,  "), sep = '_'))"  )
 }
 
 
