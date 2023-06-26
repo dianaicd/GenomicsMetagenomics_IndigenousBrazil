e6f4c8fa4ae2fffa85d8e6ae4ee1493242400c61
dianaicd <dianaivette.cd@gmail.com>
2023-06-26 21:36:10 +0200

    clean

--- is_australasian.R
+++
@@ -1,8 +0,0 @@
-is_australasian <- function(pop){
-  australasians <- c("Andaman", "Papuan", "WCD05", "WCD02", "WCD03", "WCD08", "WCD01", "WCD13", "WCD04")
-  if(pop %in% australasians){
-    return("Australasian")
-  }else{
-    return("Other")
-  }
-}
